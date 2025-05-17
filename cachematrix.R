cfbb507452ba871f6471371fb5b7383f998e29cb
Meita Muthmainnah <meitamuthmainnah05@gmail.com>
2025-05-17 17:58:24 +0700

    improved code and comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,39 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
 makeCacheMatrix <- function(x = matrix()) {
   inv <- NULL ##to store inverse of the matrix
   
   set <- function(y) {
     x <<- y ##to update matrix with the new matrix
-    inv <- NULL ##to clear cached inverse (because matrix changed)
+    inv <<- NULL ##to clear cached inverse (because matrix changed)
   }
   
   get <- function() x ##return current matrix
   
   setinverse <- function(inverse) inv <<- inverse ##to save inverse to cache
-  getinverse <- function() inv ##return cached inverse
+  getinverse <- function() inv                    ##return cached inverse
   
-  list(set = set, get=get,
-       setinverse=setinverse,
-       getinverse=getinverse) ##return a list of function so matrix and inverse can be accessed
+  ##return a list of function so matrix and inverse can be accessed
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse) 
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
   ## Return a matrix that is the inverse of 'x'
+  ## if the inverse has been calculated and cached, it uses cache
+cacheSolve <- function(x, ...) {
   inv <- x$getinverse() ##to check if there's a cached inverse already
   
   if(!is.null(inv)) {
-    message("gettingg cached data") ##letting user know it's using cache
+    message("getting cached data") ##letting user know it's using cache
     return(inv) ##return the cached inverse
   }
+  
+  #if no cache, calculate the inverse
   data <- x$get() ##to get original matrix
-  inv <- solve(data, ...) ##solve the inverse
+  inv <- solve(data, ...) ##to solve the inverse
   x$setinverse(inv) ##cached it for next time
   
   inv ##return the inverse
